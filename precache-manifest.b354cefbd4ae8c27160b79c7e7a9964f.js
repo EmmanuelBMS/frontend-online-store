8a042825d4ed52cd41091f7c5badf2dc62157d95
EmmanuelBMS <acquabriall2020@gmail.com>
2022-11-16 19:07:55 -0300

    Updates

---
+++ precache-manifest.b354cefbd4ae8c27160b79c7e7a9964f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c633e10709bd24a5159378791e77ecb",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "8910175f8ef872adde03",
+    "url": "/frontend-online-store/static/css/main.fb35bd9e.chunk.css"
+  },
+  {
+    "revision": "5ac908e748d24b4b4916",
+    "url": "/frontend-online-store/static/js/2.f7b16f77.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.f7b16f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8910175f8ef872adde03",
+    "url": "/frontend-online-store/static/js/main.e3c18a6d.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  }
+]);
